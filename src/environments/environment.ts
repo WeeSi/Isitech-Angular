3a44c96484acee03ef6dc32c7af797c70b59bd5c
WeeSi <francklucasehui@gmail.com>
2023-08-28 16:12:49 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ytihdgvsandabhjszutn.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0aWhkZ3ZzYW5kYWJoanN6dXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyMjY1MzQsImV4cCI6MjAwODgwMjUzNH0.cG-YTAd5CZb0k-lD3L9BGIz9Yok2spvWkjXf3QeW3kQ',
+};
